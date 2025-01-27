b2a1529b16f30e583f3e4f20a706c50643694a6a
Jeff Ripke <jripke74@gmail.com>
2025-01-27 11:53:55 -0800

    add break

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,6 +15,7 @@ const enteredValue = prompt("Maximum life for you and the monster.", "100");
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let lastLoggedEntry;
 
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
@@ -216,11 +217,17 @@ function printLogHandler() {
   for (let i = 0; i < 3; i++) {
     console.log("------------------");
   }
-  // for (let i = 0; i < battleLog.length; i++) {
-  //   console.log(battleLog[i]);
-  // }
+  let i = 0;
   for (const logEntry of battleLog) {
-    console.log(logEntry);
+    if ((!lastLoggedEntry && lastLoggedEntry !== 0) || lastLoggedEntry < i) {
+      console.log(`#${i}`);
+      for (const key in logEntry) {
+        console.log(`${key} => ${logEntry[key]}`);
+      }
+      lastLoggedEntry = i;
+      break;
+    }
+    i++;
   }
 }
 
